595714526db04438e5913b540798498a16f2f546
rane <rane.hs@gmail.com>
2012-03-07 17:33:05 +0900

    moved modules to bcl/

--- general.h
+++
@@ -1,9 +0,0 @@
-#define MEM_NAME_SPVMEM     "SPVMEM"    //監視領域共有メモリ名
-#define MEM_NAME_CMDMEM     "CMDMEM"    //指示共有メモリ名
-#define MEM_NAME_SPVMEM_W   L"SPVMEM"    //監視領域共有メモリ名
-#define MEM_NAME_CMDMEM_W   L"CMDMEM"    //指示共有メモリ名
-
-#define BD_IDLE   0     //アイドル状態
-#define BD_EXIT   1     //処理部終了指示
-#define BD_SHOW   2     //コンソール表示指示
-#define BD_HIDE   3     //コンソール非表示指示
